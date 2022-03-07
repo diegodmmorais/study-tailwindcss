959a5810f020bf2c43f23ac11ff76e24b2e2f274
Diego Morais <diego.dm.morais@gmail.com>
2022-03-07 00:20:35 -0300

    exemplos de text

--- next-tailwind-1/README.md
+++ next-tailwind-1/README.md
@@ -1,2 +1,3 @@
 # Comando para criar um projeto nextjs com tailwindcss
 ``` > npx create-next-app -e with-tailwindcss next-tailwind-1 ```
+``` > yarn add -D tailwindcss@latest postcss@latest autoprefixer@latest ```
--- next-tailwind-1/pages/index.tsx
+++ next-tailwind-1/pages/index.tsx
@@ -3,7 +3,12 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
-      <p className="text-2xl text-red-100">Hello Word!</p>
+      <p className="text-xs text-indigo-500">text-xs</p>
+      <p className="text-sm text-blue-500">text-sm</p>
+      <p className="text-lg text-purple-500">text-lg</p>
+      <p className="text-xl text-green-500">text-xl</p>
+      <p className="text-2xl text-gray-500">text-2xl</p>
+      <p className="text-9xl text-red-500">text-9xl</p>
     </>
   );
 };
