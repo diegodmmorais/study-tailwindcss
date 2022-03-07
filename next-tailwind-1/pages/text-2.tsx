255a5cd0ca61f367072ab7b8938299e8b8a6961a
Diego Morais <diego.dm.morais@gmail.com>
2022-03-07 00:57:32 -0300

    tudo em tailwind é baseado em class

--- next-tailwind-1/pages/text-2.tsx
+++ next-tailwind-1/pages/text-2.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next';
-
-const Home: NextPage = () => {
+const Text2 = () => {
   return (
     <>
-      <div className="bg-indigo-500 hover:bg-blue-500">
+       <div className="bg-red-500 hover:bg-blue-500">
         <h1 className="cursor-pointer p-10 text-center text-9xl font-bold italic text-white hover:font-light hover:underline">
           Bem-vindo!
         </h1>
@@ -12,4 +10,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default Text2;
