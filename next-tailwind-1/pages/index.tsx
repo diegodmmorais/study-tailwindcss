e2cd492528a5e39d5bfdc1f5730953783a59ea74
Diego Morais <diego.dm.morais@gmail.com>
2022-03-07 00:49:50 -0300

    brincando com text v1

--- next-tailwind-1/pages/index.tsx
+++ next-tailwind-1/pages/index.tsx
@@ -3,23 +3,8 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
-      <p className="text-xs text-indigo-500">text-xs</p>
-      <p className="text-sm text-blue-500">text-sm</p>
-      <p className="text-lg text-purple-500">text-lg</p>
-      <p className="text-xl text-green-500">text-xl</p>
-      <p className="text-2xl text-gray-500">text-2xl</p>
-      <p className="text-9xl text-red-500">text-9xl</p>
-      <div className="bg-indigo-500">
-        <p className="p-10 text-center font-bold text-white">Olá</p>
-      </div>
-      <div className="bg-lime-500">
-        <p className="p-10 text-left font-bold text-white">Olá</p>
-      </div>
-      <div className="bg-pink-500">
-        <p className="p-10 text-right font-bold text-white">Olá</p>
-      </div>
-      <div className="bg-red-600">
-        <p className="p-10 text-justify font-bold text-white">Olá</p>
+      <div className="bg-indigo-700">
+        <h1 className="p-10 text-center text-9xl font-bold italic text-white">Bem-vindo!</h1>
       </div>
     </>
   );
---
+++ next-tailwind-1/pages/text-1.tsx
@@ -0,0 +1,26 @@
+const Text = () => {
+  return (
+    <>
+      <p className="text-xs text-indigo-500">text-xs</p>
+      <p className="text-sm text-blue-500">text-sm</p>
+      <p className="text-lg text-purple-500">text-lg</p>
+      <p className="text-xl text-green-500">text-xl</p>
+      <p className="text-2xl text-gray-500">text-2xl</p>
+      <p className="text-9xl text-red-500">text-9xl</p>
+      <div className="bg-indigo-500">
+        <p className="p-10 text-center font-bold text-white">Olá</p>
+      </div>
+      <div className="bg-lime-500">
+        <p className="p-10 text-left font-bold text-white">Olá</p>
+      </div>
+      <div className="bg-pink-500">
+        <p className="p-10 text-right font-bold text-white">Olá</p>
+      </div>
+      <div className="bg-red-600">
+        <p className="p-10 text-justify font-black italic text-white underline">Olá</p>
+      </div>
+    </>
+  );
+};
+
+export default Text;
