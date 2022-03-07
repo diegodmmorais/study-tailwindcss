ef1a569e914f032a8c4484027f45d7020c2c2645
Diego Morais <diego.dm.morais@gmail.com>
2022-03-07 00:41:00 -0300

    brincando com text v1

--- next-tailwind-1/pages/index.tsx
+++ next-tailwind-1/pages/index.tsx
@@ -9,6 +9,18 @@ const Home: NextPage = () => {
       <p className="text-xl text-green-500">text-xl</p>
       <p className="text-2xl text-gray-500">text-2xl</p>
       <p className="text-9xl text-red-500">text-9xl</p>
+      <div className="bg-indigo-500">
+        <p className="p-10 text-center font-bold text-white">Olá</p>
+      </div>
+      <div className="bg-lime-500">
+        <p className="p-10 text-left font-bold text-white">Olá</p>
+      </div>
+      <div className="bg-pink-500">
+        <p className="p-10 text-right font-bold text-white">Olá</p>
+      </div>
+      <div className="bg-red-600">
+        <p className="p-10 text-justify font-bold text-white">Olá</p>
+      </div>
     </>
   );
 };
