6905a30ea50f7ac41852121905b0300611ba0e64
Diego Morais <diego.dm.morais@gmail.com>
2022-03-08 00:24:18 -0300

    create size v1

---
+++ next-tailwind-1/pages/size-1.tsx
@@ -0,0 +1,9 @@
+const Size = () => {
+  return (
+    <>
+      <div className="m-2 h-4 w-4 rounded bg-green-500 md:h-8 md:w-8 md:rounded md:bg-green-600 lg:h-16 lg:w-16 lg:rounded lg:bg-green-700 xl:h-32 xl:w-32 xl:rounded xl:bg-green-800"></div>
+    </>
+  );
+};
+
+export default Size;
