dc44680a3d802469d3f0f2233ce6657dcc516791
Diego Morais <diego.dm.morais@gmail.com>
2022-03-08 00:03:39 -0300

    create button v1

---
+++ next-tailwind-1/pages/button-1.tsx
@@ -0,0 +1,12 @@
+const Button = () => {
+  return (
+    <>
+      <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-10 rounded my-1 mx-1" >LIMPAR</button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded my-1 mx-1" >PESQUISAR</button>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 rounded my-1 mx-1" >SALVAR</button>
+    </>
+  );
+};
+
+
+export default Button;
