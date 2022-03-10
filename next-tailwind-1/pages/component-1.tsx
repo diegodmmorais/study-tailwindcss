95e003793d614ddc474e40a55c63dabcc4aeee6a
Diego Morais <diego.dm.morais@gmail.com>
2022-03-10 07:57:16 -0300

    componentizando o css

---
+++ next-tailwind-1/pages/component-1.tsx
@@ -0,0 +1,13 @@
+const Component = () => {
+  return (
+    <>
+      <div className="btn-container">
+        <button className="btn-default">Limpar</button>
+        <button className="btn-red ">Cancelar</button>
+        <button className="btn-greem">Salvar</button>
+      </div>
+    </>
+  );
+};
+
+export default Component;
