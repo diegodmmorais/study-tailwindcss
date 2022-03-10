49477fbce49081c8fb787a031bc22bc4acd48f3b
Diego Morais <diego.dm.morais@gmail.com>
2022-03-10 20:21:48 -0300

    applicando box-border e box-contant

---
+++ next-tailwind-1/pages/border-box-content.tsx
@@ -0,0 +1,11 @@
+const BorderBoxContent = () => {
+  return (
+    <>
+      <div className="container mx-auto bg-gradient-to-l from-cyan-500 to-cyan-900 text-white m-4 box-border p-10 border-4 border-cyan-400">Box border</div>
+
+      <div className="container mx-auto bg-gradient-to-l from-cyan-500 to-cyan-900 text-white m-4 box-content p-10 border-4 border-cyan-400">box content</div>
+    </>
+  );
+};
+
+export default BorderBoxContent;
