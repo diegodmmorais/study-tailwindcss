5ec8887a85937b34365fddfefea8e5b58c8ca3f9
Diego Morais <diego.dm.morais@gmail.com>
2022-03-10 07:15:04 -0300

    como trabalhar com foco em um input

---
+++ next-tailwind-1/pages/input-1.tsx
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <>
+      <input className="bg-gray-100 text-gray-700 focus:bg-pink-600 focus:text-white" placeholder="Clique em mim" />
+    </>
+  );
+};
+
+export default Input;
