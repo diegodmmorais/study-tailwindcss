6f39d3408a119511ff60e3358ed83fced4048cc3
Diego Morais <diego.dm.morais@gmail.com>
2022-03-10 20:55:02 -0300

    applicando display div com inline-block

---
+++ next-tailwind-1/pages/display-2.tsx
@@ -0,0 +1,13 @@
+const Display = () => {
+  return (
+    <>
+      <div className="container flex mx-auto bg-white shadow-md my-2 shadow-gray-400">
+        <div className="bg-gradient-to-l from-blue-300 to-purple-800 text-white p-3 m-4 shadow-md shadow-gray-400 inline-block flex-auto">Div 1</div>
+        <div className="bg-gradient-to-l from-pink-300 to-fuchsia-800 text-white p-3 m-4 shadow-md shadow-gray-400 inline-block flex-auto">Div 2</div>
+        <div className="bg-gradient-to-l from-cyan-300  to-fuchsia-800 text-white p-3 m-4 shadow-md shadow-gray-400 inline-block flex-auto">Div 3</div>
+      </div>
+    </>
+  );
+};
+
+export default Display;
