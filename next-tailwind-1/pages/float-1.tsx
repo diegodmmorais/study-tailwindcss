dace78937dcbf441225378ea2671b6ac9200e39e
Diego Morais <diego.dm.morais@gmail.com>
2022-03-10 21:12:21 -0300

    applicando float-right

---
+++ next-tailwind-1/pages/float-1.tsx
@@ -0,0 +1,13 @@
+const Float = () => {
+  return (
+    <>
+      <div className="container mx-auto bg-indigo-300">
+        <button className="float-right m-2 rounded bg-gradient-to-l from-teal-700 to-teal-600 p-4 text-white shadow-sm shadow-gray-800">
+          SALVAR
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Float;
